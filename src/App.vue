987dbf00b409fe8d5f4d9ca23aa8549abf94656d
Martin Grossmann <martin@vyvoj.net>
2018-10-15 23:20:08 +0200

    Initial State

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!-- the router outlet, where all matched components would ber viewed -->
+        <router-link v-bind:to="'/'">Home</router-link>
+        <router-link v-bind:to="'/about'">About</router-link>
+        <router-view>
+        </router-view>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
+<!-- styling for the component -->
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import Hello from './components/HelloWorld';
+import About from './components/About';
+import Article from './components/Article';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+const routes = [
+  {path: '/', component: Hello},
+  {path: '/about', component: About},
+  {path: '/article/:id', component: Article, props: true},
+];
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history',
+});
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  template: '<App/>',
+  components: {App},
+  router,
+}).$mount('#app');
