7d13d9f69c8782c221cc8bc177d17ae96357d2e1
Martin Grossmann <martin@vyvoj.net>
2018-10-19 19:36:52 +0200

    Setup Moment to get use proper locale, add date formatting filter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 import App from './App';
+import moment from 'moment';
 import router from './router';
 
+const locale = window.navigator.userLanguage || window.navigator.language;
+import('moment/locale/' + locale.slice(0, 2) + '.js');
+moment.locale(locale);
 
+Vue.filter('formatDate', function (value) {
+  if (value) {
+    return moment(String(value)).format('LLL');
+  }
 });
 
 new Vue({
